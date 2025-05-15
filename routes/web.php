d8e6ef1fd51ea36fdb00677156eb150d839971e2
Farrel Aditya Rosyidi <farreladitya003@gmail.com>
2025-05-15 15:56:08 +0700

    Pertemuan 11

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+//import java.io;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//System.out.println("Hello World");
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return " <h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
